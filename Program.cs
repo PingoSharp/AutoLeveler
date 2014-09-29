2fb3b0b2468f899c78fef0e48c07706f1c530070
Angelo S. <boyangel90@hotmail.it>
2014-09-29 17:04:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -38,6 +38,9 @@ namespace AutoLeveler
                 case "Ezreal":
                     AbilitySequence = new int[] { 0, 2, 0, 1, 0, 3, 0, 2, 0, 2, 3, 2, 2, 1, 1, 3, 1, 1 };
                     break;
+                case "Gnar":
+                    AbilitySequence = new int[] { 0, 1, 2, 0, 0, 3, 0, 1, 0, 1, 3, 1, 1, 2, 2, 3, 2, 2 };
+                    break;
                 case "Graves":
                     AbilitySequence = new int[] { 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 3, 2, 2, 2, 1, 3, 1, 1 };
                     break;
