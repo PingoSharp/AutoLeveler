ea5231515f7b4bcccadf4fc82e188477ee11ab4e
Angelo S. <boyangel90@hotmail.it>
2014-11-04 03:46:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -50,6 +50,9 @@ namespace AutoLeveler
                 case "Jinx":
                     AbilitySequence = new int[] { 0, 1, 2, 0, 0, 3, 0, 1, 0, 1, 3, 1, 1, 2, 2, 3, 2, 2 };
                     break;
+                case "Katarina":
+                    AbilitySequence = new int[] { 0, 2, 1, 0, 0, 3, 0, 1, 0, 1, 3, 1, 1, 2, 2, 3, 2, 2 };
+                    break;
                 case "KogMaw":
                     AbilitySequence = new int[] { 1, 2, 1, 0, 1, 3, 1, 0, 1, 0, 3, 0, 0, 2, 2, 3, 2, 2 };
                     break;
@@ -76,6 +79,9 @@ namespace AutoLeveler
                     break;
                 case "TwistedFate":
                     AbilitySequence = new int[] { 1, 0, 0, 2, 0, 3, 0, 1, 0, 1, 3, 1, 1, 2, 2, 3, 2, 2 };
+                    break;
+                case "Twitch":
+                    AbilitySequence = new int[] { 2, 1, 2, 0, 2, 3, 0, 2, 0, 2, 3, 0, 0, 1, 1, 3, 1, 1 };
                     break;
                 case "Vayne":
                     AbilitySequence = new int[] { 0, 1, 2, 1, 1, 3, 0, 0, 0, 0, 3, 1, 1, 2, 2, 3, 2, 2 };
